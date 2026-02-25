88076cb8a8b1e1c1530d776720be0f4a71725fe3
Joshua Horowitz <joshuah@alum.mit.edu>
2026-02-25 00:03:58 -0800

    lazy-load docs page so big stuff (Monaco, etc.) isn't loaded by default

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes } from "react-router-dom";
 import { autoRoute } from "./autoRoute";
 import { DemoPage } from "./demo/DemoPage";
 import { SingleDemoPage } from "./demo/SingleDemoPage";
 import { DocsIndexPage } from "./docs/DocsIndexPage";
-import { DocsPage } from "./docs/DocsPage";
 import "./index.css";
 import { IndexPage } from "./IndexPage";
 import { SingleStudyPage } from "./study/SingleStudyPage";
 import { StudyPage } from "./study/StudyPage";
 
+// We load this one lazily so it gets split into a separate bundle;
+// it's got Monaco, Babel, Prettier, etc.
+const DocsPage = React.lazy(() =>
+  import("./docs/DocsPage").then((m) => ({ default: m.DocsPage })),
+);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
-        {autoRoute("/", IndexPage)}
-        {autoRoute("/study", StudyPage)}
-        {autoRoute("/study/:id", SingleStudyPage)}
-        {autoRoute("/docs", DocsIndexPage)}
-        {autoRoute("/docs/:slug", DocsPage)}
-        {autoRoute("/demos", DemoPage)}
-        {autoRoute("/demos/:id", SingleDemoPage)}
-      </Routes>
+      <Suspense>
+        <Routes>
+          {autoRoute("/", IndexPage)}
+          {autoRoute("/study", StudyPage)}
+          {autoRoute("/study/:id", SingleStudyPage)}
+          {autoRoute("/docs", DocsIndexPage)}
+          {autoRoute("/docs/:slug", DocsPage)}
+          {autoRoute("/demos", DemoPage)}
+          {autoRoute("/demos/:id", SingleDemoPage)}
+        </Routes>
+      </Suspense>
     </HashRouter>
   </React.StrictMode>,
 );
