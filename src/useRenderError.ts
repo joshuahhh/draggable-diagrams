0ccb923e49ce917d075e5af71dbc56f3d1f8c9b9
Joshua Horowitz <joshuah@alum.mit.edu>
2026-04-08 22:35:22 -0700

    potential perf fix in useRenderError

--- src/useRenderError.ts
+++ src/useRenderError.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useThrowRenderError() {
   const [error, setError] = useState<unknown>(null);
@@ -7,26 +7,29 @@ export function useThrowRenderError() {
     throw error;
   }
 
-  return (err: unknown) => {
+  return useCallback((err: unknown) => {
     setError(err);
     throw err;
-  };
+  }, []);
 }
 
 export function useCatchToRenderError() {
   const throwRenderError = useThrowRenderError();
 
-  return function catchToRenderError<TArgs extends any[], TReturn>(
-    fn: (...args: TArgs) => TReturn,
-  ) {
-    return (...args: TArgs) => {
-      try {
-        return fn(...args);
-      } catch (error) {
-        return throwRenderError(error);
-      }
-    };
-  };
+  return useCallback(
+    function catchToRenderError<TArgs extends any[], TReturn>(
+      fn: (...args: TArgs) => TReturn,
+    ) {
+      return (...args: TArgs) => {
+        try {
+          return fn(...args);
+        } catch (error) {
+          return throwRenderError(error);
+        }
+      };
+    },
+    [throwRenderError],
+  );
 }
 
 export type CatchToRenderError = ReturnType<typeof useCatchToRenderError>;
